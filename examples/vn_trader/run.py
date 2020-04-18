0236b796a452fcd294d9701a85524a0f2ba73574
muxian.wu <willard9107@gmail.com>
2020-04-18 09:17:29 +0800

    add ctp in vn trade

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -4,53 +4,53 @@ from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
-# from vnpy.gateway.binance import BinanceGateway
-# from vnpy.gateway.bitmex import BitmexGateway
-# from vnpy.gateway.futu import FutuGateway
-# from vnpy.gateway.ib import IbGateway
-# from vnpy.gateway.ctp import CtpGateway
-# from vnpy.gateway.ctptest import CtptestGateway
-# from vnpy.gateway.mini import MiniGateway
-# from vnpy.gateway.sopt import SoptGateway
-# from vnpy.gateway.minitest import MinitestGateway
-# from vnpy.gateway.femas import FemasGateway
-# from vnpy.gateway.tiger import TigerGateway
-# from vnpy.gateway.oes import OesGateway
-# from vnpy.gateway.okex import OkexGateway
-# from vnpy.gateway.huobi import HuobiGateway
-# from vnpy.gateway.bitfinex import BitfinexGateway
-# from vnpy.gateway.onetoken import OnetokenGateway
-# from vnpy.gateway.okexf import OkexfGateway
-# from vnpy.gateway.okexs import OkexsGateway
+from vnpy.gateway.binance import BinanceGateway
+from vnpy.gateway.bitmex import BitmexGateway
+from vnpy.gateway.futu import FutuGateway
+from vnpy.gateway.ib import IbGateway
+from vnpy.gateway.ctp import CtpGateway
+from vnpy.gateway.ctptest import CtptestGateway
+from vnpy.gateway.mini import MiniGateway
+from vnpy.gateway.sopt import SoptGateway
+from vnpy.gateway.minitest import MinitestGateway
+from vnpy.gateway.femas import FemasGateway
+from vnpy.gateway.tiger import TigerGateway
+from vnpy.gateway.oes import OesGateway
+from vnpy.gateway.okex import OkexGateway
+from vnpy.gateway.huobi import HuobiGateway
+from vnpy.gateway.bitfinex import BitfinexGateway
+from vnpy.gateway.onetoken import OnetokenGateway
+from vnpy.gateway.okexf import OkexfGateway
+from vnpy.gateway.okexs import OkexsGateway
 from vnpy.gateway.xtp import XtpGateway
-# from vnpy.gateway.hbdm import HbdmGateway
+from vnpy.gateway.hbdm import HbdmGateway
 from vnpy.gateway.tap import TapGateway
-# from vnpy.gateway.tora import ToraGateway
-# from vnpy.gateway.alpaca import AlpacaGateway
-# from vnpy.gateway.da import DaGateway
-# from vnpy.gateway.coinbase import CoinbaseGateway
-# from vnpy.gateway.bitstamp import BitstampGateway
-# from vnpy.gateway.gateios import GateiosGateway
-# from vnpy.gateway.bybit import BybitGateway
-# from vnpy.gateway.deribit import DeribitGateway
+from vnpy.gateway.tora import ToraGateway
+from vnpy.gateway.alpaca import AlpacaGateway
+from vnpy.gateway.da import DaGateway
+from vnpy.gateway.coinbase import CoinbaseGateway
+from vnpy.gateway.bitstamp import BitstampGateway
+from vnpy.gateway.gateios import GateiosGateway
+from vnpy.gateway.bybit import BybitGateway
+from vnpy.gateway.deribit import DeribitGateway
 from vnpy.gateway.uft import UftGateway
-# from vnpy.gateway.okexo import OkexoGateway
-# from vnpy.gateway.binancef import BinancefGateway
+from vnpy.gateway.okexo import OkexoGateway
+from vnpy.gateway.binancef import BinancefGateway
 
-# from vnpy.app.cta_strategy import CtaStrategyApp
+from vnpy.app.cta_strategy import CtaStrategyApp
 # from vnpy.app.csv_loader import CsvLoaderApp
-# from vnpy.app.algo_trading import AlgoTradingApp
-# from vnpy.app.cta_backtester import CtaBacktesterApp
-# from vnpy.app.data_recorder import DataRecorderApp
-# from vnpy.app.risk_manager import RiskManagerApp
-# from vnpy.app.script_trader import ScriptTraderApp
-# from vnpy.app.rpc_service import RpcServiceApp
-# from vnpy.app.spread_trading import SpreadTradingApp
-# from vnpy.app.portfolio_manager import PortfolioManagerApp
-# from vnpy.app.option_master import OptionMasterApp
-# from vnpy.app.chart_wizard import ChartWizardApp
-# from vnpy.app.excel_rtd import ExcelRtdApp
-# from vnpy.app.data_manager import DataManagerApp
+from vnpy.app.algo_trading import AlgoTradingApp
+from vnpy.app.cta_backtester import CtaBacktesterApp
+from vnpy.app.data_recorder import DataRecorderApp
+from vnpy.app.risk_manager import RiskManagerApp
+from vnpy.app.script_trader import ScriptTraderApp
+from vnpy.app.rpc_service import RpcServiceApp
+from vnpy.app.spread_trading import SpreadTradingApp
+from vnpy.app.portfolio_manager import PortfolioManagerApp
+from vnpy.app.option_master import OptionMasterApp
+from vnpy.app.chart_wizard import ChartWizardApp
+from vnpy.app.excel_rtd import ExcelRtdApp
+from vnpy.app.data_manager import DataManagerApp
 from vnpy.app.portfolio_strategy import PortfolioStrategyApp
 
 
@@ -63,7 +63,7 @@ def main():
     main_engine = MainEngine(event_engine)
 
     # main_engine.add_gateway(BinanceGateway)
-    # main_engine.add_gateway(CtpGateway)
+    main_engine.add_gateway(CtpGateway)
     # main_engine.add_gateway(CtptestGateway)
     # main_engine.add_gateway(MiniGateway)
     # main_engine.add_gateway(SoptGateway)
